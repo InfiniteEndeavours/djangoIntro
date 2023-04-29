159763f912de5f0f521cb0cb7cd7c7490a071110
nyx <ethanpitt01@protonmail.com>
2023-04-29 20:43:44 +0100

    Update Project
    models.py
    - Add Item class
    
    Migrations
    - Add Item class to Migrations
    
    admin.py
    - Expose Item model on admin dashboard
    
    views.py
    - Add comment

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+# Expose the Item class to the Django admin interface
+admin.site.register(Item)
--- todo/views.py
+++ todo/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# Create a view that will return a list of to do items
 def get_todo_list(request):
     return render(request, 'todo/todo_list.html')
