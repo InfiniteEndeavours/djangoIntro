122e0fdf7b3f13bef6d621fa0f45132e56e822ee
nyx <ethanpitt01@protonmail.com>
2023-04-30 16:06:52 +0100

    Update app todo
    Update tests to cover all code

--- todo/test_views.py
+++ todo/test_views.py
@@ -48,3 +48,12 @@ class TestViews(TestCase):
         # Check if the item was toggled
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        # Simulate post request to the edit item page
+        response = self.client.post(f"/edit/{item.id}", {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        # Check if the item was edited
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
