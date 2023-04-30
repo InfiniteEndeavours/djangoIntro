2d71e5cc8c784fc4b68e396f3508ca6e9a4eb1c7
nyx <ethanpitt01@protonmail.com>
2023-04-30 15:02:54 +0100

    Update Project
    Create view for adding an item

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Item
 
 
 # Create your views here.
 
 # Create a view that will return a list of to do items
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+# Create a view to add an item to the Todo List
+def add_item(request):
+    return render(request, 'todo/add_item.html')
