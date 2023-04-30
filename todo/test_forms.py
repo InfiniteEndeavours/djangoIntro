af6a8f93c25b8738d87a722f793953a3ac915803
nyx <ethanpitt01@protonmail.com>
2023-04-30 15:44:46 +0100

    Update app todo
    Update test_forms.py

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -1,9 +1,27 @@
 from django.test import TestCase
+from .forms import ItemForm
 
 
 # Create your tests here.
 
-class TestDjango(TestCase):
+class TestItemForm(TestCase):
 
-    def test_ths_thing_works(self):
-        self.assertEqual(1, 1)
+    def test_item_name_is_required(self):
+        # Test that the name field is required
+        form = ItemForm({'name': ''})
+        # Check if the form is valid
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        # Check if the error message is correct
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_is_not_required(self):
+        # Test that the done field is not required
+        form = ItemForm({'name': 'Test Todo Item'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        # Test that the fields are explicitly specified in the metaclass
+        form = ItemForm()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
+
