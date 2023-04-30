4f85d99325a7e5a8807104b1af7125e3bc771a22
nyx <ethanpitt01@protonmail.com>
2023-04-30 15:12:30 +0100

    Update Project
    Add POST method to add_item view
    
    Add form to todo/templates/add_item.html

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 
@@ -15,4 +15,10 @@ def get_todo_list(request):
 
 # Create a view to add an item to the to do List
 def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
     return render(request, 'todo/add_item.html')
